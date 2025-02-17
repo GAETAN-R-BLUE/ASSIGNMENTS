1dece756bc96f3b4db44bae2f05f0b4f23286855
GAETAN-R-BLUE <gaetanrodrigue_98@yahoo.com>
2025-02-16 20:50:46 -0500

    changes

--- src/Lab6_12.java
+++ src/Lab6_12.java
@@ -7,20 +7,24 @@ public class Lab6_12 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter the number in your list:");
-        String numbers = input.nextLine();
+        System.out.println("Enter the number in your list:"); // Prompts the user input
+        String numbers = input.nextLine(); // Accepts the user input as a string variable
+        System.out.println();
+        System.out.println("You entered: "+ numbers); // Displays the user inputs
+        String[] Array = numbers.split(" "); // splitting the string into an array
         System.out.println();
 
-        System.out.println("You entered: "+ numbers);
-        String[] Array = numbers.split(" ");
-        System.out.println();
+        double [] newArray = new double[Array.length]; // Declaring the new array that will house my integers
 
-        double [] newArray = new double[Array.length];
         for(int i = 0; i < newArray.length; i++) {
-            newArray[i] = Double.parseDouble(Array[i]);
+            newArray[i] = Double.parseDouble(Array[i]); // converting the String array into double array
         }
-            double sum = 0;
-            double largest = newArray[0];
+            double sum = 0; // sum
+            double largest = newArray[0]; // initializing the largest value with the first number in the newArray
+
+        // The following calculate the sum in
+        // newArray and determine the largest
+
             for(int i = 0 ; i < newArray.length; i++){
                sum += newArray[i];
                if(largest < newArray[i]){
