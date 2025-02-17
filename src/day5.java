a02f148b6f3c3f43cfba529c256b4366c754492b
GAETAN-R-BLUE <gaetanrodrigue_98@yahoo.com>
2025-02-16 22:12:58 -0500

    changes

--- src/day5.java
+++ src/day5.java
@@ -1,22 +1,26 @@
+/* **************************************************************
+ * This program prompts the user for a sentence, count the number
+ * of words in the sentence and display the words count.
+ * Rodrigue Gaetan
+ * 01/01/1111
+ ****************************************************************/
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class day5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter the word to be counted:");
-        String word = input.nextLine();
-        System.out.println();
-        System.out.println("Your word has " + word.length() + " character");
-        System.out.println(word.charAt(0));
-        for(int i =0; i<word.length();i++){
-            char c = word.charAt(i);
-         //   StringTokenizer w = new StringTokenizer(word,"");
-        //    String w = word.nextToken();
+        System.out.println("Enter your sentence here:"); // Prompt the user for the sentence
+        String sentence = input.nextLine(); // Accept the user input
+        System.out.println(); //goes to the next line
 
-            System.out.print(c);
+        String [] array = sentence.split(" "); //Splitting the sentence into words
+        int count = 0; // counting the number of words in my sentence
+        for(int i =0; i < array.length; i++){
+            count++;
         }
+        System.out.println("Your sentence has "+ count + " words");
+
         input.close();
     }
 }
