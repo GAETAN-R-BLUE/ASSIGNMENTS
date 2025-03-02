0c44459796bc0c253e213803e6416616ef3bc8b9
GAETAN-R-BLUE <gaetanrodrigue_98@yahoo.com>
2025-03-02 12:53:06 -0500

    changes

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
-
 public class FizzBuzz {
+
     public static void main(String[] args) {
         System.out.println("Enter a positive integer:");
         int userInput = new Scanner(System.in).nextInt();
 
         for( int i = 1; i <= userInput; i++ ){
             if (i % 15 == 0){
-                System.out.println("FIZZ BUZZ");
+                System.out.println("FIZZBUZZ");
             }
              else if (i % 5 == 0){
                 System.out.println("BUZZ");
@@ -18,7 +18,6 @@ public class FizzBuzz {
             else {
                 System.out.println(i);
             }
-
         }
     }
 }
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,20 +1,32 @@
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ This program checks whether a given string is a palindrome. It uses
+ replaceAll to remove all special characters, replaces whitespace, and
+  converts the string to lowercase. A for loop is then used to reverse
+  the string and compare it to the original, determining if the input
+  is a palindrome.
+ *************************************************************************/
 import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your sentence"); // prompts the user for the sentence input
 
-        System.out.println("Enter a word to be checked:");
-        String word = input.nextLine();
-        System.out.println(word);
-        System.out.println();
-
-        int size = word.length();
-
-        for (int i = size - 1; i >= 0; i--){
-            System.out.print(word.charAt(i));
+       // Accepts the user input.
+        String  sentence = new Scanner(System.in).nextLine().toLowerCase().replaceAll("[^a-zA-Z0-9 ]","").replace(" ","");
+        System.out.println("Your sentence without special character and space:");
+        System.out.println(sentence);
+        String newSentence = "";
+        for (int i = sentence.length()-1; i >= 0; i--){
+            newSentence += sentence.charAt(i);
         }
-        String newword = "i";
-        System.out.println(newword);
+        System.out.println(newSentence);
+        if (newSentence.equals(sentence)){
+            System.out.println(sentence + " is a palindrome");
+        }
+        else {
+            System.out.println(sentence + " is not a palindrome");
+        }
     }
 }
+
+
