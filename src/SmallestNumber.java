8018001c5299390291527a620e708a3be9040950
GAETAN-R-BLUE <gaetanrodrigue_98@yahoo.com>
2025-03-05 20:21:54 -0500

    changes

--- src/SmallestNumber.java
+++ src/SmallestNumber.java
@@ -8,22 +8,15 @@ import java.util.Scanner; // Importing the scanner object
 public class SmallestNumber {
     public static void main(String[] args) {
 
-        System.out.println("Enter 3 number integer separated with coma:"); // Prompts the user for the numbers
-        String [] numbersArray = new Scanner(System.in).nextLine().split(","); // splitting the user input
-        int [] numArray = new int[numbersArray.length]; // declaring a new array where I will store my variable
-
-        // the next for loop attributes all the element of my array of type int, and find the smallest
-        for(int i = 0; i < numbersArray.length; i++) {
-            numArray[i] = Integer.parseInt(numbersArray[i]);
-        }
-        int smallest = numArray[0]; // initializing my smallest value with the first element of my array
-            for(int i = 0; i < numbersArray.length; i++) {
-
-                if (smallest > numArray[i]) {
-                    smallest = numArray[i];
-                }
+        System.out.println("Enter your number integer separated with coma:"); // Prompts the user for the numbers
+        String [] userInput = new Scanner(System.in).nextLine().split(",");
+        int smallest = Integer.parseInt(userInput[0]);
+        for(int i=0; i < userInput.length; i++){
+            int currentNum = Integer.parseInt(userInput[i]);
+            if(currentNum < smallest){
+                smallest = currentNum;
             }
-        System.out.println( "The smallest number is: " + smallest);
-
+        }
+        System.out.println("The smallest number in you array is:" + smallest);
     }
 }
--- src/SumOfDigit.java
+++ src/SumOfDigit.java
@@ -1,17 +1,12 @@
 import java.util.Scanner;
-
 public class SumOfDigit {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter a positive integer with at least 2 digits");
-        String  number = input.nextLine();
-
-        int sum = 0;
-        for(int i = 0; i < number.length(); i++) {
-
-            sum += Integer.parseInt(String.valueOf(number.charAt(i)));
+        String  userInput = new Scanner(System.in).nextLine();
+        int number = 0;
+        for(int i = 0; i < userInput.length();i++){
+        number += Integer.parseInt(String.valueOf(userInput.charAt(i)));
         }
-        System.out.println("The sum of the your number is: " + sum );
-      input.close();
+       System.out.println(("The sum of the digits of your number is: " + number));
     }
 }
