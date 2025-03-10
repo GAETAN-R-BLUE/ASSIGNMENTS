54a56a85b3a307a9df49eb9731266ed566f0aff4
GAETAN-R-BLUE <gaetanrodrigue_98@yahoo.com>
2025-03-10 18:47:31 -0400

    changes

--- src/FizzBuzzEnhanced.java
+++ src/FizzBuzzEnhanced.java
@@ -1,3 +1,4 @@
+//import java.util.ArrayList;
 //import java.util.HashMap;
 //import java.util.Scanner;
 //
@@ -5,9 +6,13 @@
 //    public static void main(String[] args) {
 //        System.out.println(" Enter the multiples, the word to print for each multiple, and the last number in the range. Example: '3 fizz 5 buzz 4 fuzz 40");
 //        String [] userInput =new Scanner(System.in).nextLine().split(" ");
-//        HashMap <String, Integer > inputs = new HashMap<>();
-//        for (int i = 0; i < userInput.length; i++){
-//            inputs.put(userInput[i],userInput[inputs] );
+//        //       ArrayList <Integer> numbers = new ArrayList<>();
+////        for (int i = 0; i <= userInput.length; i++){
+////
+////        }
+//        HashMap <Integer, String > inputs = new HashMap<>();
+//        for (String input : userInput ){
+//        inputs.put(input,inputs.get(input));
 //        }
 //
 //    }
