0d208a5845f1f4c9d9d1edf158d5b9afb53e7b01
GAETAN-R-BLUE <gaetanrodrigue_98@yahoo.com>
2025-03-11 18:51:56 -0400

    changes

--- src/QueueIntro.java
+++ src/QueueIntro.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class QueueIntro {
     public static void main(String[] args) {
         System.out.println("Enter the elements of your queue:");
-        Queue<String> myQueue = new LinkedList<>(Arrays.asList(new Scanner(System.in).nextLine().split(" ")));
+        Queue<String> myQueue = new LinkedList<>(Arrays.asList(new Scanner(System.in).nextLine().split(" ")));// I do not know how I came up with this, but I surely did not use AI.
         System.out.println(myQueue);
         System.out.println(myQueue.remove());// remove and return the first element in m,y queue
         System.out.println(myQueue.element());
