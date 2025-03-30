82326955fa401973a4cddb7afe8a34051779c270
GAETAN-R-BLUE <gaetanrodrigue_98@yahoo.com>
2025-03-29 22:18:07 -0400

    changes

---
+++ src/Method.java
@@ -0,0 +1,17 @@
+//import java.util.Scanner;
+//
+//public class Method {
+//    public static void main(String[] args) {
+//        System.out.println("Enter 2 number separated with a coma:");
+//
+//        String[] number = new Scanner(System.in).nextLine().split(",");
+//        add(Integer.parseInt(number[0]), Integer.parseInt(number[1]));
+//
+//    }
+//
+//    static public int add(int num1, int num2) {
+//        int result = num1 + num2;
+//        System.out.println(result);
+//        return result;
+//    }
+//}
